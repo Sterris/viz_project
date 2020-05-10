66ebfc66b6310ad688674d8edb831f14c66fca9c
Luca <dorigoluca@gmail.com>
2020-05-10 14:11:54 +0200

    Add files to generate website

---
+++ notebooks/compile.sh
@@ -0,0 +1,14 @@
+#!/usr/local/bin/bash
+
+jupyter nbconvert --to slides All_Visualisations_02_05.ipynb --template="reveal_test.tpl"
+
+cp "All_Visualisations_02_05.slides.html" ../../test_pages/index.html
+
+cd ../../test_pages
+
+git add index.html
+
+git commit -m "Update index"
+
+git push
+
